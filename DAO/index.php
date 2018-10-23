7b30708841d1df832caa052fd04360edb11849ba
GeovannePad <geovannepadilha@hotmail.com>
2018-10-23 01:18:06 -0300

    DAO - Data Acess Object

---
+++ DAO/index.php
@@ -0,0 +1,43 @@
+<?php
+
+  require_once("config.php");
+
+  //$sql = new Sql();
+
+  //$usuarios = $sql->select("SELECT * FROM tb_usuarios");
+
+  //var_dump($usuarios);
+
+  // Carrega 1 usuário
+  //$root = new Usuario();
+  //$root->loadById(5);
+  //echo $root;
+
+  // Carrega uma lista de usuários
+  //$lista = Usuario::getList();
+  //echo json_encode($lista);
+
+  // Carrega uma lista de usuários buscando pelo login
+  //$search = Usuario::search("jo");
+  //echo json_encode($search);
+
+  // Carrega um usuário pelo login e senha.
+  //$usuario = new Usuario();
+  //$usuario->login("Barriquelo", "!@#$");
+  //echo $usuario;
+
+  // Criando um novo usuário
+  //$aluno = new Usuario("aluno", "@lun0");
+  //$aluno->insert();
+  //echo $aluno;
+
+  // Alterar um usuário
+  //$usuario = new Usuario();
+  //$usuario->loadById(6);
+  //$usuario->update("Professor", "!@#$%");
+
+  // Deletando um usuário
+  //$usuario = new Usuario();
+  //$usuario->loadById(6);
+  //$usuario->delete();
+  //echo $usuario;
